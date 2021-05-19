1e89d1ceda08da54f764fddac4c41af86bce4c4c
sabuj sen <sen.sabuj82@gmail.com>
2021-05-19 14:22:16 +0530

    Add project files.

---
+++ DsAlgo/Program.cs
@@ -0,0 +1,39 @@
+﻿ using System;
+
+namespace DsAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            var r= TwoSum(nums, 9);
+        }
+
+        /// <summary>
+        /// https://leetcode.com/problems/two-sum/submissions/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum(int[] nums, int target)
+        {
+
+            int cNum = 0;
+            int[] outt = new int[2] { -1,-1};
+            while (cNum != nums.Length )
+            {
+                for (int i = cNum; i < nums.Length; i++)
+                {
+                    if (nums[i] + nums[cNum] == target && i!=cNum)
+                    {
+                        outt[0] = i;
+                        outt[1] = cNum;
+                    }
+                }
+                cNum++;
+            }
+            return outt;
+        }
+    }
+}
